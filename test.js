91bde553277573b950eed80e42bdc06a0086e5aa
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-18 09:55:41 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+function printHeader() {
+    console.log("Header");
+    console.log("......");
+    console.log("......");
+}
+function printMessage() {
+    console.log();
+    console.log("This is first step to code");
+    console.log("One code can be written from different coders");
+
+}
+function printFooter() {
+    console.log(`This is your final result`);
+    console.log(`Learned at SoftUni js-book online`);
+}
+
+function printTotal() {
+    printHeader();
+    printMessage();
+    printFooter();
+}
+printTotal();
+
